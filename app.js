9940d0d428a730cfa60e48ea9aad8cb6a07459fb
austinogiza <austinogiza@gmail.com>
2021-07-07 00:49:32 +0100

    element changed

--- app.js
+++ app.js
@@ -1,14 +1,101 @@
 
+const initialize =  `
+
+<link rel="stylesheet" type="text/css"  href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605461/css/console_mksvog.css">
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605461/css/dracula.min_graxc3.css">
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605465/css/reset.min_ihxu18.css">
+
+<link rel="shortcut icon" href="https://res.cloudinary.com/verifiedly/image/upload/v1625613183/favicon_f5k5ae.png" type="image/x-icon" />
+<!-- Bootstrap 4.5 -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605462/css/bootstrap.min_l5tfnu.css" type="text/css" />
+<!-- animate -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625613146/css/animate_l5mser.css" type="text/css" />
+<!-- Swiper -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605471/css/swiper.min_xi214w.css" />
+<!-- icons -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605462/css/icons_vdegdz.css" type="text/css" />
+<!-- aos -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605460/css/aos_b6mqul.css" type="text/css" />
+<!-- main css -->
+<link rel="stylesheet" href="./assets/css/main.css" type="text/css" />
+<!-- normalize -->
+<link rel="stylesheet" href="https://res.cloudinary.com/verifiedly/raw/upload/v1625605464/css/normalize_vjmkir.css" type="text/css" />
+
+
+
+<body class="bg-snow  h-100vh  d-flex position-relative  flex flex-column justify-content-center align-item-center">
+
+
+<div class="container">
+<a class="btn btn_md_primary redirect_btn padding-px-4 padding-py-2 margin-t-1 margin-b-1  bg-blue rounded-12 c-white h-fit-content">
+Verify with Verifiedly
+</a>
+
+
+  <div class="kyc_modal_background">
+<div class="kyc_modal">
+
+</div>
+  </div>
+</div>
+  <!-- jquery -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605454/js/jquery-3.5.0_inc3z0.js" type="text/javascript"></script>
+  <!-- jquery-migrate -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605453/js/jquery-migrate.min_gcnjzt.js" type="text/javascript"></script>
+  <!-- popper -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/popper.min_lb4jbh.js" type="text/javascript"></script>
+  <!-- bootstrap -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605452/js/bootstrap.min_klzrbk.js" type="text/javascript"></script>
+  <!--
+  ============
+  vendor file
+  ============
+   -->
+  <!-- particles -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605457/js/vendor/particles.min_fhbnsj.js" type="text/javascript"></script>
+  <!-- TweenMax -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605458/js/vendor/TweenMax.min_sbb0pr.js" type="text/javascript"></script>
+  <!-- ScrollMagic -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605458/js/vendor/ScrollMagic_maenwt.js" type="text/javascript"></script>
+  <!-- animation.gsap -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/animation.gsap_hln4sr.js" type="text/javascript"></script>
+  <!-- addIndicators -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/debug.addIndicators.min_ed7lew.js" type="text/javascript"></script>
+  <!-- Swiper js -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605458/js/vendor/swiper.min_ggjmrz.js" type="text/javascript"></script>
+  <!-- countdown -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/countdown_rkm6v5.js" type="text/javascript"></script>
+  <!-- simpleParallax -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605457/js/vendor/simpleParallax.min_lelhtg.js" type="text/javascript"></script>
+  <!-- waypoints -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605458/js/vendor/waypoints.min_s1k8uw.js" type="text/javascript"></script>
+  <!-- counterup -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/jquery.counterup.min_ifepdt.js" type="text/javascript"></script>
+  <!-- charming -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/charming.min_kxgeny.js" type="text/javascript"></script>
+  <!-- imagesloaded -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/imagesloaded.pkgd.min_qin0k4.js" type="text/javascript"></script>
+  <!-- BX-Slider -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/jquery.bxslider.min_ya1lyo.js" type="text/javascript"></script>
+  <!-- Sharer -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605457/js/vendor/sharer_nrvblc.js" type="text/javascript"></script>
+  <!-- sticky -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605457/js/vendor/sticky.min_r2i69h.js" type="text/javascript"></script>
+  <!-- Aos -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605456/js/vendor/aos_nh4eaq.js" type="text/javascript"></script>
+  <!-- main file -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625605454/js/main_feltmm.js" type="text/javascript"></script>
+  <!-- sass -->
+  <script src="https://res.cloudinary.com/verifiedly/raw/upload/v1625604222/sass_otc3tz.js" type="text/javascript"></script>
+
+</body>
+`
+
 //assign modal
 const kycModalBackground = document.querySelector('.kyc_modal_background')
 //assign button
 const redirectBtn = document.querySelector('.redirect_btn')
 
-
-const initialize = ()=>{
-
-
-}
 //add eventlistener to button that initializes the modal and kyc process
 redirectBtn.addEventListener('click', ()=>{
 
@@ -19,10 +106,9 @@ redirectBtn.addEventListener('click', ()=>{
 class VerifiedlyKYC extends HTMLElement {
 
     constructor(){
-
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.innerHTML = content
+        super();
+        this.attachShadow({mode:"open"});
+        this.shadowRoot.innerHTML = initialize
     }
 }
 
