61ff7b10683421326b226dca2603e937fd24e516
austinogiza <austinogiza@gmail.com>
2021-07-06 21:35:21 +0100

    added custom element

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //assign modal
 const kycModalBackground = document.querySelector('.kyc_modal_background')
 //assign button
@@ -6,10 +7,23 @@ const redirectBtn = document.querySelector('.redirect_btn')
 
 const initialize = ()=>{
 
-    
+
 }
 //add eventlistener to button that initializes the modal and kyc process
 redirectBtn.addEventListener('click', ()=>{
 
     kycModalBackground.classList.add('showModal')
 })
+
+
+class VerifiedlyKYC extends HTMLElement {
+
+    constructor(){
+
+        super()
+        this.attachShadow({mode:"open"})
+        this.shadowRoot.innerHTML = content
+    }
+}
+
+customElements.define("Verifiedly", VerifiedlyKYC)
